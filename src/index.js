311eec11cc841bdeff6f1116e5af0f2b88a2064e
T-Wesst <code.daddy17@gmail.com>
2020-11-16 10:31:04 -0800

    lazy load example

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import nav from './nav';
-import { footer } from './footer';
 import makeButton from './button';
 import { makeColorStyle } from './button-styles';
 import makeImage from './image';
 import css from './footer.css';
 import buttonStyle from './button.css';
 import imageURL from './webpack-logo.jpg';
+// import { footer } from './footer';
+const loadFooter = () => import('./footer');
 
 const button = makeButton('yay a button');
 const image = makeImage(imageURL);
 
+button.addEventListener('click', (e) => {
+  loadFooter().then((module) => {
+    document.body.appendChild(module.footer);
+  });
+});
+
 button.style = makeColorStyle('cyan');
 document.body.appendChild(button);
 document.body.appendChild(image);
-document.body.appendChild(footer);
 
 console.log(nav(), makeColorStyle('yellow'));
