557071461428e423f44d59c8bc80a428e6f0cf76
T-Wesst <code.daddy17@gmail.com>
2020-11-19 02:32:52 -0800

    test config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const appsScriptPlugin = require('gas-webpack-plugin');
 const { merge } = require('webpack-merge');
 const modeConfig = (env) => require(`./build-utils/webpack.${env}`)(env);
 const presetConfig = require('./build-utils/loadPresets');
 
+const destination = path.resolve(__dirname, 'dist');
+
 module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) => {
   return merge(
     {
-      mode: 'none',
+      mode,
+      entry: path.resolve(__dirname, './src/index.js'),
       module: {
         rules: [
           {
@@ -23,9 +31,23 @@ module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) => {
       },
       output: {
         filename: 'bundle.js',
+        path: destination,
         chunkFilename: '[name].lazy-chunk.js',
       },
-      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          inlineSource: '.(js|css)$',
+          filename: 'index.html',
+        }),
+        // new HtmlWebpackInlineSourcePlugin(),
+        new CleanWebpackPlugin(),
+        new webpack.ProgressPlugin(),
+        new appsScriptPlugin(),
+        new CopyPlugin({
+          patterns: [{ from: 'appsscript.json', to: destination }],
+        }),
+      ],
     },
     modeConfig(mode),
     presetConfig({ mode, presets })
